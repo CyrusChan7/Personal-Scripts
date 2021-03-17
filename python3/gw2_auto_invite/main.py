1cbe8c064dd1e1c14db2e0216612a0a7597b7c95
Cyrus Chan <cyruschan4561@gmail.com>
2021-03-16 21:11:07 -0700

    Added config.ini settings support

--- python3/gw2_auto_invite/main.py
+++ python3/gw2_auto_invite/main.py
@@ -1,4 +1,5 @@
 from invitefriend import InviteFriend
+from configparser import ConfigParser  # This is a built-in Python library
 import time
 import os
 import sys
@@ -10,7 +11,20 @@ if __name__ == "__main__":
     dir_name = os.path.dirname(abs_path)
     os.chdir(dir_name)
 
-    invite_people = InviteFriend("names.txt", 3)
+    # Instantiation
+    config = ConfigParser()
+    try:
+        config.read("config.ini")
+        file_name = config.get("General", "filename")
+        inv_count = config.getint("General", "friend_invite_count")
+        delay_before_exec = config.getint("General", "start_delay")
+        invite_people = InviteFriend(file_name, inv_count, delay_before_exec)
+    except:
+        print(f"config.ini file does not exist in the script's directory, or the values are incorrect. "
+              f"Exiting script in 5 seconds.")
+        time.sleep(5)
+        sys.exit()
+
     invite_people.focus_game_window()
     invite_people.invite_friends()
 
