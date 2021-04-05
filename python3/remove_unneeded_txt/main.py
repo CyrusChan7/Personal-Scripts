04d61ce6576ff5b99933baef6b0e6bf6e93ccb81
Cyrus Chan <cyruschan4561@gmail.com>
2021-04-05 09:54:43 -0700

    Reduced TxtFile method dependency on each other

--- python3/remove_unneeded_txt/main.py
+++ python3/remove_unneeded_txt/main.py
@@ -3,49 +3,48 @@ import os
 
 class TxtFile:
     def __init__(self, file_path):
+        # I will use long variable names here as I prefer to be clear
+        # on exactly what they mean, as I will be using these variables a lot later
         self._file_path = file_path
         self._txt_file_names_record = []
+        self._all_files_counter = 0
+        self._txt_file_counter = 0
+
+
+    def gather_directory_statistics(self):
+        for file in os.listdir(self._file_path):
+            self._all_files_counter += 1
+            if file.endswith(".txt"):
+                self._txt_file_counter += 1
+                self._txt_file_names_record.append(file)
+
+
+    @property
+    def txt_file_names_record(self):
+        return self._txt_file_names_record
 
 
     def display_directory_statistics(self):
-        txt_file_names = []
+        txt_file_percentage = round(self._txt_file_counter / self._all_files_counter * 100, 2)
 
-        all_files_counter = 0
-        txt_file_counter = 0
-
-        for file in os.listdir(self._file_path):
-            all_files_counter += 1
-            if file.endswith(".txt"):
-                txt_file_counter += 1
-                txt_file_names.append(file)
-
-        txt_file_percentage = round(txt_file_counter / all_files_counter * 100, 2)
-
-        print(f"\nThere are {all_files_counter} files in '{self._file_path}', of which {txt_file_counter} "
+        print(f"\nThere are {self._all_files_counter} files in '{self._file_path}', of which {self._txt_file_counter} "
               f"({txt_file_percentage}%) are .txt files.")
 
-        print(f"\nNames of the .txt files in this directory: {txt_file_names}")
-
-        self._txt_file_names_record = txt_file_names
+        print(f"\nNames of the .txt files in this directory: {self._txt_file_names_record}")
 
 
-    def display_file_content(self, read_line_count=7):
+    def display_file_content(self, txt_file, read_line_count=7):
         # 5 may not be enough lines to understand the context of the file, 10 is too spammy in the console,
         # 7 as a default is good
-        progress_counter = 0
-        for txt_file in self._txt_file_names_record:
-            with open(txt_file) as f:
-                head = f.readlines()[0:read_line_count]
-            #print(head)
+        with open(txt_file) as f:
+            head = f.readlines()[0:read_line_count]
+        #print(head)
 
-            print("")
-            print("=" * 75)
-            print(f"\nFile contents in {txt_file}:\n")
-            for line in head:
-                print(line.strip())
-            self.ask_user_for_decision(txt_file)
-            progress_counter += 1
-            print(f"You have processed {progress_counter} .txt file(s). There are {len(self._txt_file_names_record) - progress_counter} .txt file(s) remaining.\n")
+        print("")
+        print("=" * 75)
+        print(f"File contents in {txt_file}:\n")
+        for line in head:
+            print(line.strip())
 
 
     def ask_user_for_decision(self, file_name):
@@ -74,5 +73,11 @@ if __name__ == "__main__":
     os.chdir(dir_name)
 
     folder_cleanup = TxtFile(dir_name)
+    folder_cleanup.gather_directory_statistics()
     folder_cleanup.display_directory_statistics()
-    folder_cleanup.display_file_content()
+
+    for file in folder_cleanup.txt_file_names_record:
+        folder_cleanup.display_file_content(file, 7)
+        folder_cleanup.ask_user_for_decision(file)
+
+    print("All .txt files have been processed.")
