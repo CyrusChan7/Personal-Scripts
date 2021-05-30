a3ab553c46b6908f3623698d4c8894c096dd7b27
Cyrus Chan <cyruschan4561@gmail.com>
2021-05-30 16:30:39 -0700

    Add RAM output - display_computer_specs

--- python3/display_computer_specs/main.py
+++ python3/display_computer_specs/main.py
@@ -2,7 +2,15 @@ import platform
 import psutil
 
 
-def display_computer_info():
+def convert_bytes_to_GB(bytes_number):
+    """ Convert a number in bytes to a number in GB, then round to 4 digits """
+    GB_number = bytes_number / (1024 ** 3)
+    GB_number = round(GB_number, 4)
+    return GB_number
+
+
+def display_basic_info():
+    """ A cross-platform way to display the basic information on a computer """
 
     print(f"Computer's network name: {platform.node()}")
     print(f"OS name: {platform.system()}")
@@ -14,8 +22,23 @@ def display_computer_info():
     print(f"Physical CPU count: {psutil.cpu_count(logical=False)}")
 
 
+def display_RAM_info():
+    """ A cross-platform way to display the RAM information on a computer"""
+
+    installed_RAM = psutil.virtual_memory().total
+    available_RAM = psutil.virtual_memory().free
+    used_RAM = psutil.virtual_memory().used
+
+    installed_RAM_GB = convert_bytes_to_GB(installed_RAM)
+    available_RAM_GB = convert_bytes_to_GB(available_RAM)
+    used_RAM_GB = convert_bytes_to_GB(used_RAM)
+
+    print(f"Installed RAM: {installed_RAM_GB} GB (Used: {used_RAM_GB} GB. Available: {available_RAM_GB} GB)")
+
+
 def main():
-    display_computer_info()
+    display_basic_info()
+    display_RAM_info()
 
 
 if __name__ == "__main__":
